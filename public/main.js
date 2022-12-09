811d82f065f207b45e65ba6bdf88b580f35cd46d
Corson Shane <corson.shane@student.sl.on.ca>
2022-12-09 12:37:21 -0500

    fixed pagination and started coding page display

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
-
-function getData(page=0, take=10){
+let page = 0;
+let take = 5;
+function getData(){
     fetch(`/restaurantsJson?page=${page}&take=${take}`)
         .then(response => response.json())
         .then( results =>{
@@ -15,4 +16,47 @@ function getData(page=0, take=10){
     })
 }
 
+function updatePageData(){
+    /*fetch(`/restaurantsJson?page=${page}&take=${take}`)
+        .then(response => response.json())
+        .then( results =>{
+    let pageData = "";
+    pageData += "Displaying ";
+    if(page=1){
+        pageData += "1"
+    }
+    else{
+        pageData += take+1;
+    }
+    pageData += " - ";
+    pageData += page+1 * take;
+    pageData += " of ";
+    pageData += "PLACEHOLDER";
+    document.querySelector(".rowsDisplay").innerHTML = pageData;
+    })*/
+}
+
 getData();
+updatePageData();
+
+document.querySelector(".previous").addEventListener("click", event => {
+    if(page!=0){
+        page--;
+    }
+    getData();
+    updatePageData();
+});
+
+document.querySelector(".next").addEventListener("click", event => {
+    page++;
+    getData();
+    updatePageData();
+});
+
+document.querySelector("#perPage").addEventListener("change", event => {
+    page = 0;
+    let perPageValue = document.querySelector('#perPage option:checked').value;
+    take = perPageValue;
+    getData();
+    updatePageData();
+});
